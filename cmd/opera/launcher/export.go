b59ceaf7ba8b1bcd5dc46a967f0517a36443edba
Egor Lysenko <uprendis@gmail.com>
2022-06-10 00:55:11 +0400

    add command for exporting preimages

--- cmd/opera/launcher/export.go
+++ cmd/opera/launcher/export.go
@@ -33,6 +33,25 @@ var (
 // always print out progress. This avoids the user wondering what's going on.
 const statsReportLimit = 8 * time.Second
 
+func exportPreimages(ctx *cli.Context) error {
+	if len(ctx.Args()) < 1 {
+		utils.Fatalf("This command requires an argument.")
+	}
+
+	cfg := makeAllConfigs(ctx)
+
+	rawProducer := integration.DBProducer(path.Join(cfg.Node.DataDir, "chaindata"), cfg.cachescale)
+	gdb, err := makeRawGossipStore(rawProducer, cfg)
+	if err != nil {
+		log.Crit("DB opening error", "datadir", cfg.Node.DataDir, "err", err)
+	}
+	defer gdb.Close()
+
+	fn := ctx.Args().First()
+
+	return utils.ExportPreimages(gdb.EvmStore().EvmDb, fn)
+}
+
 func exportEvents(ctx *cli.Context) error {
 	if len(ctx.Args()) < 1 {
 		utils.Fatalf("This command requires an argument.")
