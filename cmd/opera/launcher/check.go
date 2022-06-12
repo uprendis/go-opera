d791f4b6f9d3cd64bc02bc648751663d15031fe5
Egor Lysenko <uprendis@gmail.com>
2022-06-12 10:54:45 +0400

    f add support of multi

--- cmd/opera/launcher/check.go
+++ cmd/opera/launcher/check.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/Fantom-foundation/lachesis-base/kvdb"
+	"github.com/Fantom-foundation/lachesis-base/kvdb/multidb"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
@@ -15,11 +16,27 @@ import (
 	"github.com/Fantom-foundation/go-opera/inter"
 )
 
-func makeRawDbsProducer(cfg *config) kvdb.FullDBProducer {
+func makeRawDbsProducers(cfg *config) map[multidb.TypeName]kvdb.IterableDBProducer {
 	dbsList, err := integration.SupportedDBs(path.Join(cfg.Node.DataDir, "chaindata"), cfg.DBs.Cache)
 	if err != nil {
 		utils.Fatalf("Failed to initialize DB producers: %v", err)
 	}
+	if err := checkStateInitialized(dbsList); err != nil {
+		utils.Fatalf(err.Error())
+	}
+	return dbsList
+}
+
+func makeMultiRawDbsProducer(dbsList map[multidb.TypeName]kvdb.IterableDBProducer, cfg *config) kvdb.FullDBProducer {
+	multiRawDbs, err := integration.MakeRawMultiProducer(dbsList, cfg.DBs.Routing)
+	if err != nil {
+		utils.Fatalf("Failed to initialize multi DB producer: %v", err)
+	}
+	return multiRawDbs
+}
+
+func makeRawDbsProducer(cfg *config) kvdb.FullDBProducer {
+	dbsList := makeRawDbsProducers(cfg)
 	multiRawDbs, err := integration.MakeRawMultiProducer(dbsList, cfg.DBs.Routing)
 	if err != nil {
 		utils.Fatalf("Failed to initialize multi DB producer: %v", err)
