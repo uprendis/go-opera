8e9cce2eaa2af2fd12c81b67b20b95eebd82b122
Egor Lysenko <uprendis@gmail.com>
2022-12-06 08:27:49 +0400

    f

--- utils/compactdb/compactdb.go
+++ utils/compactdb/compactdb.go
@@ -3,6 +3,8 @@ package compactdb
 import (
 	"bytes"
 	"math/big"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/Fantom-foundation/lachesis-base/kvdb"
@@ -54,18 +56,67 @@ func addToPrefix(prefix *big.Int, diff *big.Int, size int) []byte {
 	return append(res, end...)
 }
 
-func Compact(unprefixedDB kvdb.Store, loggingName string) error {
-	lastLog := time.Time{}
-	compact := func(db kvdb.Store, b int, start, end []byte) error {
-		if len(loggingName) != 0 && time.Since(lastLog) > time.Second*16 {
-			log.Info("Compacting DB", "name", loggingName, "until", hexutils.BytesToHex(append([]byte{byte(b)}, end...)))
-			lastLog = time.Now()
-		}
-		return db.Compact(start, end)
+type loggedStore struct {
+	kvdb.Store
+	lastLog time.Time
+	name    string
+
+	currentOp atomic.Value
+
+	wg   sync.WaitGroup
+	quit chan struct{}
+}
+
+func (s *loggedStore) Compact(start []byte, limit []byte) error {
+	s.currentOp.Store(limit)
+	err := s.Store.Compact(start, limit)
+	if err != nil {
+		log.Error("Compaction error", "name", s.name, "err", err)
+		return err
 	}
+	return nil
+}
+
+func (s *loggedStore) StartLogging() {
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		ticker := time.NewTicker(16 * time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				untilI := s.currentOp.Load()
+				if untilI != nil {
+					until := untilI.([]byte)
+					untilStr := hexutils.BytesToHex(until)
+					if until == nil {
+						untilStr = "end"
+					}
+					log.Info("Compacting DB", "name", s.name, "until", untilStr)
+				}
+			case <-s.quit:
+				return
+			}
+		}
+	}()
+}
+
+func (s *loggedStore) StopLogging() {
+	close(s.quit)
+	s.wg.Wait()
+}
+
+func Compact(unprefixedDB kvdb.Store, loggingName string) error {
+	loggedDB := &loggedStore{
+		Store: unprefixedDB,
+		name:  loggingName,
+		quit:  make(chan struct{}),
+	}
+	loggedDB.StartLogging()
+	defer loggedDB.StopLogging()
 
 	for b := 0; b < 256; b++ {
-		prefixed := table.New(unprefixedDB, []byte{byte(b)})
+		prefixed := table.New(loggedDB, []byte{byte(b)})
 		first := firstKey(prefixed)
 		if first == nil {
 			continue
@@ -74,18 +125,21 @@ func Compact(unprefixedDB kvdb.Store, loggingName string) error {
 		if last == nil {
 			continue
 		}
+		println(b, hexutils.BytesToHex(first), hexutils.BytesToHex(last))
 		keySize := len(last)
 		if keySize < len(first) {
 			keySize = len(first)
 		}
 		first = common.RightPadBytes(first, keySize-len(first))
 		last = common.RightPadBytes(last, keySize-len(last))
+		println(b, hexutils.BytesToHex(first), hexutils.BytesToHex(last))
 		firstBn := new(big.Int).SetBytes(first)
 		lastBn := new(big.Int).SetBytes(last)
 		diff := new(big.Int).Sub(lastBn, firstBn)
+		println(b, firstBn.String(), lastBn.String(), diff.String())
 		if diff.Cmp(big.NewInt(10000)) < 0 {
 			// short circuit if too few keys
-			err := compact(prefixed, b, nil, nil)
+			err := prefixed.Compact(nil, nil)
 			if err != nil {
 				return err
 			}
@@ -94,7 +148,7 @@ func Compact(unprefixedDB kvdb.Store, loggingName string) error {
 		var prev []byte
 		for i := 32; i >= 1; i-- {
 			until := addToPrefix(firstBn, new(big.Int).Div(diff, big.NewInt(int64(i))), keySize)
-			err := compact(prefixed, b, prev, until)
+			err := prefixed.Compact(prev, until)
 			if err != nil {
 				return err
 			}
