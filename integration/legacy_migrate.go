77fd795c49ac20628a96ab4cd0c7e324584c7289
Egor Lysenko <uprendis@gmail.com>
2023-03-04 02:03:06 +0400

    f

--- integration/legacy_migrate.go
+++ integration/legacy_migrate.go
@@ -19,6 +19,7 @@ import (
 	"github.com/syndtr/goleveldb/leveldb/opt"
 
 	"github.com/Fantom-foundation/go-opera/utils/dbutil/autocompact"
+	"github.com/Fantom-foundation/go-opera/utils/dbutil/compactdb"
 )
 
 func lastKey(db kvdb.Store) []byte {
@@ -46,7 +47,7 @@ type transformTask struct {
 
 func transform(m transformTask) error {
 	openDst := func() *batched.Store {
-		return batched.Wrap(autocompact.Wrap(m.openDst(), 32*opt.GiB))
+		return batched.Wrap(autocompact.Wrap(m.openDst(), 1*opt.GiB))
 	}
 	openSrc := func() *batched.Store {
 		return batched.Wrap(m.openSrc())
@@ -114,9 +115,9 @@ func transform(m transformTask) error {
 		keys = keys[:0]
 	}
 	// compact the new DB
-	//if err := compactdb.Compact(dst, m.name, 16*opt.GiB); err != nil {
-	//	return err
-	//}
+	if err := compactdb.Compact(dst, m.name, 16*opt.GiB); err != nil {
+		return err
+	}
 	return nil
 }
 
