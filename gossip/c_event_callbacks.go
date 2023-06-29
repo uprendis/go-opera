73b1af31d8aff025652fef4f88abf5e099ea6108
Egor Lysenko <uprendis@gmail.com>
2023-06-30 03:25:23 +0400

    f

--- gossip/c_event_callbacks.go
+++ gossip/c_event_callbacks.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math/big"
 	"sync/atomic"
+	"time"
 
 	"github.com/Fantom-foundation/lachesis-base/gossip/dagprocessor"
 	"github.com/Fantom-foundation/lachesis-base/hash"
@@ -174,8 +175,14 @@ func (s *Service) EvmSnapshotGeneration() bool {
 	return gen
 }
 
+var eventT time.Duration
+
 // processEvent extends the engine.Process with gossip-specific actions on each event processing
 func (s *Service) processEvent(e *inter.EventPayload) error {
+	start := time.Now()
+	defer func() {
+		eventT += time.Since(start)
+	}()
 	// s.engineMu is locked here
 	if s.stopped {
 		return errStopped
@@ -242,6 +249,7 @@ func (s *Service) processEvent(e *inter.EventPayload) error {
 	}
 
 	if newEpoch != oldEpoch {
+		println("==+=", "eventT", eventT.String(), "blockT", blockT.String())
 		s.switchEpochTo(newEpoch)
 	}
 
